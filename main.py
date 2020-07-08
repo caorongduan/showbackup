6c29a905bccba6c7809be2666666842a917a209b
caord <caord@showwant.com>
2020-07-08 11:22:19 +0800

    add example.conf.json

--- main.py
+++ main.py
@@ -11,8 +11,14 @@ License: Apache2, see LICENSE for more details.
 Version: 1.0
 """
 
+import json
 from showops.mysql import backup
 
 
 if __name__ == "__main__":
-    backup()
+    mysql_conf = None
+    with open("./conf.json", "r") as conf_json:
+        conf_dict = json.loads(conf_json.read())
+        mysql_conf = conf_dict["mysql"]
+
+    backup(mysql_conf)
--- showops/mysql.py
+++ showops/mysql.py
@@ -15,70 +15,79 @@ mysql相关功能
 
 import os
 import time
-from config import MysqlConfig
 from showops.utils import create_not_exists
 
 
-def make_dump_cmd(path, db_name=None, table=None):
+def run_mysqldump(usr, pwd, host, port, backup_path, is_zip, db_name=None, table=None):
     """ 产生mysqldump命令，支持全库，单库，数据表备份语句
     """
-    dump_cmd_temp = (
-        "mysqldump -u{username} -p{password} {params} {source} > {target}"
-    )
+    dump_cmd_temp = "mysqldump -u{usr} -p{pwd} -h{host} -P{port} {params} {source} > {target}"
     if db_name is None and table is None:
         # all databases backup
         source = ""
-        target = os.path.join(path, "all_databases.sql")
+        target = os.path.join(backup_path, "all_databases.sql")
         default_params = "-A -B -F -R --master-data=2"
     elif table is not None:
         # tables backup
         source = "{} {}".format(db_name, table)
-        target = os.path.join(path, "table_{}.sql".format(table))
+        target = os.path.join(backup_path, "table_{}.sql".format(table))
         default_params = ""
     elif db_name is not None:
         # one database backup
         source = "{}".format(db_name)
-        target = os.path.join(path, "db_{}.sql".format(db_name))
+        target = os.path.join(backup_path, "db_{}.sql".format(db_name))
         default_params = "-B -F -R --master-data=2"
 
-    if MysqlConfig.is_gzip:
+    if is_zip:
         source = "{}|gzip".format(source)
         target = "{}.gz".format(target)
 
     cmd = dump_cmd_temp.format(
-        username=MysqlConfig.user,
-        password=MysqlConfig.password,
-        params=default_params,
-        source=source,
-        target=target,
+        usr=usr, pwd=pwd, host=host, port=port, params=default_params, source=source, target=target
     )
-    return cmd
+    # os.system(cmd)
+    print(cmd)
 
 
-def backup():
+def backup(conf):
     # 定义备份文件夹生成规则 eg.'/backup/20170817_123205/database_name'
-    today_path = os.path.join(
-        MysqlConfig.backup_path, time.strftime("%Y%m%d_%H%M%S")
-    )
-    if not MysqlConfig.source:
+    today_path = os.path.join(conf["backup_path"], time.strftime("%Y%m%d_%H%M%S"))
+    if not conf["source"]:
         # all databases backup if the DB_TARGET is empty
-        path = os.path.join(today_path, "all_databases")
-        create_not_exists(path)
-        cmd = make_dump_cmd(path)
+        backup_path = os.path.join(today_path, "all_databases")
+        create_not_exists(backup_path)
         print("Starting backup of all databases")
-        return os.system(cmd)
+        run_mysqldump(
+            conf["usr"], conf["pwd"], conf["host"], conf["port"], backup_path, conf["is_zip"]
+        )
+        return
 
-    for target in MysqlConfig.source:
+    for target in conf["source"]:
         db_name = target["db"]
         tables = target.get("tables", [])
-        path = os.path.join(today_path, db_name)
-        create_not_exists(path)
+        backup_path = os.path.join(today_path, db_name)
+        create_not_exists(backup_path)
         if len(tables) > 0:
             for table in tables:
-                cmd = make_dump_cmd(path, db_name, table)
                 print("Starting backup of table {}".format(table))
-                os.system(cmd)
+                run_mysqldump(
+                    conf["usr"],
+                    conf["pwd"],
+                    conf["host"],
+                    conf["port"],
+                    backup_path,
+                    conf["is_zip"],
+                    db_name,
+                    table,
+                )
         else:
-            cmd = make_dump_cmd(path, db_name)
             print("Starting backup of database {}".format(db_name))
-            os.system(cmd)
+            run_mysqldump(
+                conf["usr"],
+                conf["pwd"],
+                conf["host"],
+                conf["port"],
+                backup_path,
+                conf["is_zip"],
+                db_name,
+            )
